361af83d763b9b5ded94e02678c6bfaa4c47b575
Umut Utkan <umut.utkan@gmail.com>
2007-12-22 18:38:18 +0000

    google analytics added

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -26,5 +26,16 @@
         <div id="footer">
             Copyright &copy; 2007 Hoydaa Inc.
         </div>
+        
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-794867-4");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>        
+        
     </body>
 </html>
