300ab9d5413b1cb5cb35bccb2e6d75f5f3796aee
Umut Utkan <umut.utkan@gmail.com>
2008-02-20 13:44:08 +0000

    kullanici ekleme ve guncelleme sirasinda sadece sifre alani doldurulmus ise sifrenin olusturulmasi ve guncellenmesi icin gerekli kod eklendi

--- apps/backend/modules/user/actions/actions.class.php
+++ apps/backend/modules/user/actions/actions.class.php
@@ -4,6 +4,15 @@ class userActions extends autouserActions
 {
     protected function updateUserFromRequest()
     {
+        if(!$this->user->getId() && !$this->getRequestParameter('password')) {
+            $password = substr(md5(rand(100000, 999999)), 0, 6);
+            $this->user->setPassword($password);
+        }
+        
+        if($this->getRequestParameter('password')) {
+            $this->user->setPassword($this->getRequestParameter('password'));
+        }
+        
         parent::updateUserFromRequest();
     }
 }
