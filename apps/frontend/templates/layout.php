ce4c7c2ad73b8cb239cf555b57fb175c70f86a00
Umut Utkan <umut.utkan@gmail.com>
2008-02-20 13:50:07 +0000

    frontend layoutuna login logout linkleri eklendi

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -12,6 +12,17 @@
             	<a href="<?php echo url_for('@homepage') ?>"><?php echo image_tag('logo', 'id=logo') ?></a>
                 <?php include_partial('global/breadcrumb', array('pages' => $pages)) ?>
                 <?php include_partial('global/tabbar', array('tab' => $tab)) ?>
+                <div id="login">
+	                <?php if ($sf_user->isAuthenticated()): ?>
+						<a href="<?php echo url_for('user/profile') ?>"><span><?php echo $sf_user->getUsername() ?>'s profile</span></a>
+						| <a href="<?php echo url_for('user/logout') ?>"><span>logout</span></a>
+	                    <?php if($sf_user->hasCredential('admin')): ?>
+							| <?php echo link_to('admin', '../backend_dev.php/user', array('target'=>'_blank')) ?>
+	                    <?php endif; ?>
+                    <?php else: ?>
+    					<a href="<?php echo url_for('user/login') ?>"><span>login</span></a>
+    				<?php endif; ?>
+				</div>
             </div>
         </div>
         <div id="main">
@@ -27,15 +38,15 @@
             Copyright &copy; 2007 Hoydaa Inc.
         </div>
         
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-794867-4");
-pageTracker._initData();
-pageTracker._trackPageview();
-</script>        
+		<script type="text/javascript">
+			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+			document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+		<script type="text/javascript">
+			var pageTracker = _gat._getTracker("UA-794867-4");
+			pageTracker._initData();
+			pageTracker._trackPageview();
+		</script>        
         
     </body>
 </html>
