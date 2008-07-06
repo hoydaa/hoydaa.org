4ecf743fe0c5cb0c25f33db0faf693255bd08ec0
Umut Utkan <umut.utkan@gmail.com>
2008-07-06 14:18:25 +0000

    genel duzenlemeler

--- apps/frontend/modules/project/lib/projectSitemapGenerator.class.php
+++ apps/frontend/modules/project/lib/projectSitemapGenerator.class.php
@@ -3,6 +3,7 @@ class projectSitemapGenerator implements sitemapGenerator
 {
     public static function generate() {
     	$urls = array();
+    	$urls[] = new sitemapURL('projects', date('F j, Y'));
     	$projects = ProjectPeer::doSelect(new Criteria());
     	foreach($projects as $project) {
     		$urls[] = new sitemapURL('projects/'.$project->getTag(), date('F j, Y'));
