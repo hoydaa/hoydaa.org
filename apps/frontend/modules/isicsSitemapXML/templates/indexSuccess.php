a172ccbb60449e16ae4ea003051eeec9e7a86c4a
Umut Utkan <umut.utkan@gmail.com>
2008-07-06 16:45:01 +0000

    *** empty log message ***

--- apps/frontend/modules/isicsSitemapXML/templates/indexSuccess.php
+++ apps/frontend/modules/isicsSitemapXML/templates/indexSuccess.php
@@ -1,4 +1,4 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>';?>hede
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>';?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <?php foreach ($urls as $url): ?>
   <url>
