12004925ac1b0c48c760a03da3a0137cb1918ae6
Umut Utkan <umut.utkan@gmail.com>
2008-08-03 19:34:10 +0000

    HOYDAAWEB-4: Date format is changed.

--- apps/frontend/modules/project/lib/projectSitemapGenerator.class.php
+++ apps/frontend/modules/project/lib/projectSitemapGenerator.class.php
@@ -3,11 +3,11 @@ class projectSitemapGenerator implements sitemapGenerator
 {
     public static function generate() {
     	$urls = array();
-    	$urls[] = new sitemapURL('projects', date('F j, Y'));
+    	$urls[] = new sitemapURL('projects', date('M j, Y'));
     	$projects = ProjectPeer::doSelect(new Criteria());
     	foreach($projects as $project) {
-    		$urls[] = new sitemapURL('projects/'.$project->getTag(), date('F j, Y'));
-    		$urls[] = new sitemapURL('project/listDevelopers?id='.$project->getTag(), date('F j, Y'));
+    		$urls[] = new sitemapURL('projects/'.$project->getTag(), date('M j, Y'));
+    		$urls[] = new sitemapURL('project/listDevelopers?id='.$project->getTag(), date('M j, Y'));
     	}
     	return $urls;
     }
