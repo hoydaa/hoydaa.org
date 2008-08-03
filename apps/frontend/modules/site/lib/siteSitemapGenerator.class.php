d418b4dffdbe6d794bab05b06b7b3cb026d53841
Umut Utkan <umut.utkan@gmail.com>
2008-08-03 20:09:08 +0000

    HOYDAAWEB-5: Added changefreq and priority fields to sitemap.xml.

--- apps/frontend/modules/site/lib/siteSitemapGenerator.class.php
+++ apps/frontend/modules/site/lib/siteSitemapGenerator.class.php
@@ -3,7 +3,7 @@ class siteSitemapGenerator implements sitemapGenerator
 {
     public static function generate() {
     	$urls = array();
-    	$urls[] = new sitemapURL('about_us', date('M j, Y'));
+    	$urls[] = new sitemapURL('about_us', date('M j, Y'), 'monthly', 0.8);
     	return $urls;
     }
 }
