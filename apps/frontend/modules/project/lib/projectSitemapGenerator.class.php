65d4a6ac12a37380d2aac86f9675b90565295d2d
Umut Utkan <umut.utkan@gmail.com>
2008-08-06 16:24:49 +0000

    HOYDAAWEB-4: Used Y-m-d\TH:i:s\Z as date format.

--- apps/frontend/modules/project/lib/projectSitemapGenerator.class.php
+++ apps/frontend/modules/project/lib/projectSitemapGenerator.class.php
@@ -3,11 +3,11 @@ class projectSitemapGenerator implements sitemapGenerator
 {
     public static function generate() {
     	$urls = array();
-    	$urls[] = new sitemapURL('projects', date('M j, Y'), 'weekly', 1.0);
+    	$urls[] = new sitemapURL('projects', date('Y-m-d\TH:i:s\Z'), 'weekly', 1.0);
     	$projects = ProjectPeer::doSelect(new Criteria());
     	foreach($projects as $project) {
-    		$urls[] = new sitemapURL('projects/'.$project->getTag(), date('M j, Y'), 'weekly', 0.8);
-    		$urls[] = new sitemapURL('project/listDevelopers?id='.$project->getTag(), date('M j, Y'), 'weekly', 0.8);
+    		$urls[] = new sitemapURL('projects/'.$project->getTag(), date('Y-m-d\TH:i:s\Z'), 'weekly', 0.8);
+    		$urls[] = new sitemapURL('project/listDevelopers?id='.$project->getTag(), date('Y-m-d\TH:i:s\Z'), 'weekly', 0.8);
     	}
     	return $urls;
     }
--- apps/frontend/modules/projectAnnouncement/lib/projectAnnouncementSitemapGenerator.class.php
+++ apps/frontend/modules/projectAnnouncement/lib/projectAnnouncementSitemapGenerator.class.php
@@ -5,7 +5,7 @@ class projectAnnouncementSitemapGenerator implements sitemapGenerator
     	$urls = array();
     	$items = ProjectAnnouncementPeer::doSelect(new Criteria());
     	foreach($items as $item) {
-    		$urls[] = new sitemapURL('project/showAnnouncement?id='.$item->getId(), date('M j, Y', strtotime($item->getCreatedAt())), 'monthly', 1.0);
+    		$urls[] = new sitemapURL('project/showAnnouncement?id='.$item->getId(), date('Y-m-d\TH:i:s\Z', strtotime($item->getCreatedAt())), 'monthly', 1.0);
     	}
     	return $urls;
     }
